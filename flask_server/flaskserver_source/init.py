7009f39a6b6fea123a2de0410c434a4ebfcc8b4c
yeomyeom <ysy9639@naver.com>
2019-11-10 13:38:29 +0900

    flask server update

---
+++ flask_server/flaskserver_source/init.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, url_for
+from flask import request
+from flask_cors import CORS
+from urllib.request import urlopen
+from urllib.error import HTTPError
+import json
+import re
+import pickle
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+def web_parse(url):     # Function of Open HTML.
+      try:
+            html = urlopen(url).read().decode('utf-8')
+      except HTTPError as e:
+            html = None
+      return html
+
+def parse(url):  # Second Parsing of REAL URL
+      pics, stickers, texts, text = [],[],[],[]  # arrays of pic, sticker, text.
+      html = web_parse(url)
+      try:
+            #pics += re.findall('<img.*?src="(.*?)".*?data-width="(.*?)" data-height="(.*?)".*?>', secondhtml) #parse pics number
+            pics += re.findall('<img.*?src="(.*?)".*?>', html) #parse pics number
+            #stickers += re.findall('<img.*? src="(.*?)".*?class="se-sticker-image" />',secondhtml)  #parse sticker number
+            stickers += re.findall('<img.*? src="(.*?)".*?class="se-sticker-image" />',html)  #parse sticker number
+            text += re.findall('<span.*?>(.*?)</span>',html) #parse text
+          
+            for parse in text:
+                  texts += re.findall("[가-힝0-9:. ]",parse)
+            return texts
+      except:
+            return None
+
+#===============================================================================
+@app.route("/<url>")
+def template_test(url):
+      #url 받아서 파싱-완료
+      #파싱된 파일들을 가지고 점수 출력
+      #해당 점수를 가지고 html 코드 생성
+      #return render_template('bad.html')
+      url = "https://blog.naver.com/PostView.nhn?" + url
+      texts = []
+      texts = parse(url)
+      #result = " ".join(texts)
+      emotion = -0.75
+      title = 0.5
+      link = ['https://weble.com', 'https://ad.com']
+      sticker = ['https://naver.com/stickerasd', 'https://naver.com/sticker23123']
+      same = []
+      tag = 0.9
+      result ={
+            'Emotion':emotion,
+            'Title':title,
+            'Link':link,
+            'Sticker':sticker,
+            'Same':same,
+            'Tag':tag
+            }
+      jsonstr = json.dumps(result)
+      #jsonstr = result;
+      return jsonstr;
+
+@app.route("/error")
+def error():
+      result = "네이버 블로그가 아닙니다."
+      return result
+
+if __name__=="__main__":
+      app.run(port=5001)
