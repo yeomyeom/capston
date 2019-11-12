c958826a57632e7d231cf5ec24b2ba174c1693cc
yeomyeom <ysy9639@naver.com>
2019-11-12 16:59:00 +0900

    refactory

--- StickerBlock-master/StickerBlock-Release/popup.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', function (evt) {
-	chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, {
-		file: 'payload.js'
-	});
-});
-
-chrome.runtime.onMessage.addListener(function (message) {
-	document.getElementById('content').innerHTML = '스티커 ' + message + '개 제거했습니다.';
-});
